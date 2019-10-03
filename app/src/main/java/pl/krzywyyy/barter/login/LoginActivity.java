22acacabe69feb75105149e27dac351510e4fc4a
Piotr <krzywusiakk@gmail.com>
2019-10-03 10:37:56 +0200

    login layout

---
+++ app/src/main/java/pl/krzywyyy/barter/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package pl.krzywyyy.barter.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pl.krzywyyy.barter.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
