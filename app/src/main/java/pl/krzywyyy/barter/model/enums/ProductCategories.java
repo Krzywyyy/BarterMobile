e04bb9813c1d3b546ce8af93d180b90343f90af0
Piotr <krzywusiakk@gmail.com>
2019-10-17 09:05:20 +0200

    enums

---
+++ app/src/main/java/pl/krzywyyy/barter/model/enums/ProductCategories.java
@@ -0,0 +1,5 @@
+package pl.krzywyyy.barter.model.enums;
+
+public enum ProductCategories {
+    BOOK, NOTES, COACHING, OTHER
+}
---
+++ app/src/main/java/pl/krzywyyy/barter/model/enums/Specializations.java
@@ -0,0 +1,5 @@
+package pl.krzywyyy.barter.model.enums;
+
+public enum Specializations {
+    IT, MECHANICS, MECHATRONICS, CHEMISTRY, BIOLOGY, OTHER
+}
