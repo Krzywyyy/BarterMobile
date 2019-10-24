37da6149723b1cf4747c4d68d6cd60f71804abf9
Piotr <krzywusiakk@gmail.com>
2019-10-24 17:44:12 +0200

    added inject method for register fragment

--- app/src/main/java/pl/krzywyyy/barter/di/components/AppComponent.java
+++ app/src/main/java/pl/krzywyyy/barter/di/components/AppComponent.java
@@ -4,10 +4,12 @@ import javax.inject.Singleton;
 
 import dagger.Component;
 import pl.krzywyyy.barter.authentication.LoginFragment;
+import pl.krzywyyy.barter.authentication.RegisterFragment;
 import pl.krzywyyy.barter.di.modules.RetrofitModule;
 
 @Singleton
 @Component(modules = {RetrofitModule.class})
 public interface AppComponent {
     void inject(LoginFragment loginFragment);
+    void inject(RegisterFragment registerFragment);
 }
