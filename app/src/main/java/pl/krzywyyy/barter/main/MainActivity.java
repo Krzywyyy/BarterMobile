6982bc849b5c646bfce021ed0ed8d8edecdfee5e
Piotr <krzywusiakk@gmail.com>
2019-10-28 13:52:57 +0100

    sample toolbar with options menu

--- app/src/main/java/pl/krzywyyy/barter/main/MainActivity.java
+++ app/src/main/java/pl/krzywyyy/barter/main/MainActivity.java
@@ -1,10 +1,17 @@
 package pl.krzywyyy.barter.main;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import java.util.Objects;
 
 import pl.krzywyyy.barter.R;
+import pl.krzywyyy.barter.utils.TokenExplorator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.main_app_toolbar);
+        TextView username = findViewById(R.id.toolbar_username);
+        username.setText(TokenExplorator.getNameFromToken(getApplicationContext()));
+        setSupportActionBar(toolbar);
+
+        Objects.requireNonNull(getSupportActionBar()).setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_app_menu,menu);
+        return true;
     }
 }
