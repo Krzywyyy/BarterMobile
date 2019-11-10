c63d69df90787a71c185bc1277f7d37b1409cbd1
Piotr <krzywusiakk@gmail.com>
2019-11-10 22:39:29 +0100

    removed noargsconstructor from productview

--- app/src/main/java/pl/krzywyyy/barter/model/domain/ProductView.java
+++ app/src/main/java/pl/krzywyyy/barter/model/domain/ProductView.java
@@ -4,12 +4,9 @@ import android.graphics.Bitmap;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
-
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class ProductView {
     private String title;
     private Bitmap image;
