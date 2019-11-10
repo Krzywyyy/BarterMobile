d34285a653accefabd19ff45ded54af47ae5f502
Piotr <krzywusiakk@gmail.com>
2019-11-10 12:14:13 +0100

    interceptor for http logging and addid auth header to requests

--- app/src/main/java/pl/krzywyyy/barter/di/components/AppComponent.java
+++ app/src/main/java/pl/krzywyyy/barter/di/components/AppComponent.java
@@ -3,13 +3,17 @@ package pl.krzywyyy.barter.di.components;
 import javax.inject.Singleton;
 
 import dagger.Component;
+import pl.krzywyyy.barter.di.modules.RetrofitModule;
 import pl.krzywyyy.barter.ui.authentication.LoginFragment;
 import pl.krzywyyy.barter.ui.authentication.RegisterFragment;
-import pl.krzywyyy.barter.di.modules.RetrofitModule;
+import pl.krzywyyy.barter.ui.main.userproducts.UserProductsRepository;
 
 @Singleton
 @Component(modules = {RetrofitModule.class})
 public interface AppComponent {
     void inject(LoginFragment loginFragment);
+
     void inject(RegisterFragment registerFragment);
+
+    void inject(UserProductsRepository userProductsRepository);
 }
