ad53c431c1f9a68792c759120a31829eec04d258
Piotr <krzywusiakk@gmail.com>
2019-11-10 22:09:41 +0100

    home

--- app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeFragment.java
+++ app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeFragment.java
@@ -4,27 +4,42 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import pl.krzywyyy.barter.R;
+import pl.krzywyyy.barter.model.domain.ProductView;
 
 public class HomeFragment extends Fragment {
 
-    private HomeViewModel homeViewModel;
+    private List<ProductView> products = new ArrayList<>();
+    private HomeViewModel mViewModel;
+    private RecyclerView mRecyclerView;
+    private HomeAdapter productsAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        mViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
+        mRecyclerView = view.findViewById(R.id.all_products_recycler_view);
 
-        final TextView textView = root.findViewById(R.id.welcome_home_text);
-        homeViewModel.getText().observe(this, textView::setText);
+        productsAdapter = new HomeAdapter(products);
+        mRecyclerView.setAdapter(productsAdapter);
 
-        return root;
+        mViewModel.getProductViews().observe(this, productViews -> {
+            products = productViews;
+
+            productsAdapter = new HomeAdapter(products);
+            mRecyclerView.setAdapter(productsAdapter);
+        });
+
+        return view;
     }
 
 }
--- app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeViewModel.java
+++ app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeViewModel.java
@@ -3,16 +3,66 @@ package pl.krzywyyy.barter.ui.main.home;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import pl.krzywyyy.barter.MyApplication;
+import pl.krzywyyy.barter.api.ProductInterface;
+import pl.krzywyyy.barter.model.domain.Product;
+import pl.krzywyyy.barter.model.domain.ProductView;
+import pl.krzywyyy.barter.utils.ImageEncoder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.internal.EverythingIsNonNull;
+
 public class HomeViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
+    @Inject
+    Retrofit retrofit;
+
+    private MutableLiveData<List<ProductView>> productViews;
 
     public HomeViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("Witam w domu");
+        MyApplication.appComponent.inject(this);
+        productViews = getAllProducts();
     }
 
-    public MutableLiveData<String> getText() {
-        return mText;
+    public MutableLiveData<List<ProductView>> getProductViews() {
+        return productViews;
+    }
+
+    private MutableLiveData<List<ProductView>> getAllProducts() {
+        ProductInterface productService = retrofit.create(ProductInterface.class);
+
+        Call<List<Product>> call = productService.findAll(1);
+
+        MutableLiveData<List<ProductView>> products = new MutableLiveData<>();
+
+        call.enqueue(new Callback<List<Product>>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<List<Product>> call, Response<List<Product>> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    List<ProductView> productViews = new ArrayList<>();
+                    for (Product product : response.body()) {
+                        productViews.add(new ProductView(
+                                product.getTitle(),
+                                ImageEncoder.toImage(product.getImage())
+                        ));
+                    }
+                    products.setValue(productViews);
+                }
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<List<Product>> call, Throwable t) {
+            }
+        });
+        return products;
     }
 }
