389e7228619e686a2961e5fc2ed61bfa86a9af4e
Piotr <krzywusiakk@gmail.com>
2019-11-15 13:38:52 +0100

    changed variable name

--- app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeAdapter.java
+++ app/src/main/java/pl/krzywyyy/barter/ui/main/home/HomeAdapter.java
@@ -46,8 +46,8 @@ public class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.ViewHolder> {
                 FragmentTransaction fragmentTransaction = ((AppCompatActivity) context)
                         .getSupportFragmentManager().beginTransaction();
                 fragmentTransaction.addToBackStack(dialogFragmentName);
-                DialogFragment dialogFragment = new ProductDetailsFragment(productViewList.get(position).getId());
-                dialogFragment.show(fragmentTransaction, dialogFragmentName);
+                DialogFragment productDetailsDialogFragment = new ProductDetailsFragment(productViewList.get(position).getId());
+                productDetailsDialogFragment.show(fragmentTransaction, dialogFragmentName);
             });
         }
     }
