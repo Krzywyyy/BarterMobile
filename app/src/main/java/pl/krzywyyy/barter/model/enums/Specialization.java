4dfd4d384c25084760f41031f69aa76018c52bc7
Piotr <krzywusiakk@gmail.com>
2019-11-24 11:51:07 +0100

    updated enums

--- app/src/main/java/pl/krzywyyy/barter/model/enums/Specialization.java
+++ app/src/main/java/pl/krzywyyy/barter/model/enums/Specialization.java
@@ -1,5 +1,26 @@
 package pl.krzywyyy.barter.model.enums;
 
+import androidx.annotation.NonNull;
+
+import pl.krzywyyy.barter.model.dictionaries.SpecializationDictionary;
+
 public enum Specialization {
-    IT, MECHANICS, MECHATRONICS, CHEMISTRY, BIOLOGY, OTHER
+    IT(SpecializationDictionary.IT),
+    MECHANICS(SpecializationDictionary.MECHANICS),
+    MECHATRONICS(SpecializationDictionary.MECHATRONICS),
+    CHEMISTRY(SpecializationDictionary.CHEMISTRY),
+    BIOLOGY(SpecializationDictionary.BIOLOGY),
+    OTHER(SpecializationDictionary.OTHER);
+
+    private String value;
+
+    Specialization(String value){
+        this.value = value;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return value;
+    }
 }
