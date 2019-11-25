20f90fb171c0719db6aaa8d31c802a1c25ac16a5
Piotr <krzywusiakk@gmail.com>
2019-11-25 11:14:53 +0100

    method for adding product

--- app/src/main/java/pl/krzywyyy/barter/ui/main/newproduct/NewProductViewModel.java
+++ app/src/main/java/pl/krzywyyy/barter/ui/main/newproduct/NewProductViewModel.java
@@ -15,10 +15,16 @@ import lombok.Getter;
 import lombok.Setter;
 import pl.krzywyyy.barter.MyApplication;
 import pl.krzywyyy.barter.R;
+import pl.krzywyyy.barter.api.ProductInterface;
 import pl.krzywyyy.barter.model.domain.Product;
 import pl.krzywyyy.barter.model.enums.ProductCategory;
 import pl.krzywyyy.barter.model.enums.Specialization;
+import pl.krzywyyy.barter.utils.ImageEncoder;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.internal.EverythingIsNonNull;
 
 @Getter
 @Setter
@@ -40,7 +46,26 @@ public class NewProductViewModel extends BaseObservable {
         productImage = BitmapFactory.decodeResource(context.getResources(), R.drawable.no_image);
     }
 
-    public void tost(Context context) {
-        Toast.makeText(context, product.toString(), Toast.LENGTH_SHORT).show();
+    public void addNewProduct(Context context) {
+        product.setImage(ImageEncoder.toBase64(productImage));
+
+        ProductInterface productService = retrofit.create(ProductInterface.class);
+        Call<Product> call = productService.save(product);
+
+        call.enqueue(new Callback<Product>() {
+            @EverythingIsNonNull
+            @Override
+            public void onResponse(Call<Product> call, Response<Product> response) {
+                if (response.isSuccessful()) {
+                    Toast.makeText(context, R.string.successfully_added_new_product, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @EverythingIsNonNull
+            @Override
+            public void onFailure(Call<Product> call, Throwable t) {
+                Toast.makeText(context, R.string.failed_retrofit_request_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
