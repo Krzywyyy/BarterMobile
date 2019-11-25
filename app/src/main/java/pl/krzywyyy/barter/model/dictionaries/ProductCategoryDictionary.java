f3fe94491c6ebd893bc742bcd1e575f3eab43186
Piotr <krzywusiakk@gmail.com>
2019-11-25 11:44:34 +0100

    fixed name

--- app/src/main/java/pl/krzywyyy/barter/model/dictionaries/ProductCategoryDictionary.java
+++ app/src/main/java/pl/krzywyyy/barter/model/dictionaries/ProductCategoryDictionary.java
@@ -4,5 +4,5 @@ public class ProductCategoryDictionary {
     public static final String BOOK = "Książka";
     public static final String NOTES = "Notatki";
     public static final String COACHING = "Korepetycje";
-    public static final String OTHERS = "Inne";
+    public static final String OTHER = "Inne";
 }
