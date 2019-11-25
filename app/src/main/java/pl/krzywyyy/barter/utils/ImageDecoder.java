dec5dea3a6e81629b0d025a6b129359f2717db9e
Piotr <krzywusiakk@gmail.com>
2019-11-25 11:02:12 +0100

    image decoder

---
+++ app/src/main/java/pl/krzywyyy/barter/utils/ImageDecoder.java
@@ -0,0 +1,20 @@
+package pl.krzywyyy.barter.utils;
+
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class ImageDecoder {
+    public static String toBase64(Bitmap bitmap) {
+        byte[] imageByte = getBytesFromBitmap(bitmap);
+        return Base64.encodeToString(imageByte, Base64.DEFAULT);
+    }
+
+    private static byte[] getBytesFromBitmap(Bitmap bitmap) {
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, byteArrayOutputStream);
+        return byteArrayOutputStream.toByteArray();
+    }
+}
