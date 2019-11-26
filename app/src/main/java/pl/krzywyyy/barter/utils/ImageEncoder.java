9fa0966ee3341f48eddb23d31616487808ced50e
Piotr <krzywusiakk@gmail.com>
2019-11-26 09:14:46 +0100

    fixed base64 encoding

--- app/src/main/java/pl/krzywyyy/barter/utils/ImageEncoder.java
+++ app/src/main/java/pl/krzywyyy/barter/utils/ImageEncoder.java
@@ -9,7 +9,7 @@ import java.io.ByteArrayOutputStream;
 public class ImageEncoder {
     public static String toBase64(Bitmap bitmap) {
         byte[] imageByte = getBytesFromBitmap(bitmap);
-        return Base64.encodeToString(imageByte, Base64.DEFAULT);
+        return Base64.encodeToString(imageByte, Base64.NO_WRAP);
     }
 
     private static byte[] getBytesFromBitmap(Bitmap bitmap) {
